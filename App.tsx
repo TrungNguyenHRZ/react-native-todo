82bbe3363f61d5a89f5564f275a18c01478a7a03
TrungNguyenHRZ <trungnt10012k@gmail.com>
2025-07-14 16:58:53 +0700

    add numberic to todo app

--- App.tsx
+++ App.tsx
@@ -1,11 +1,45 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   const [count, setCount] = useState<number>(0);
+  const [name, setName] = useState<string>("");
+  const [age, setAge] = useState<number>(0);
+
   return (
     <View style={styles.container}>
+      <View>
+        <Text style={{ fontSize: 40, fontWeight: "600" }}>Name: {name}</Text>
+        <TextInput
+          multiline
+          autoCapitalize={"words"}
+          onChangeText={(value) => setName(value)}
+          style={{
+            borderColor: "green",
+            borderWidth: 1,
+            width: 200,
+            padding: 15,
+          }}
+        />
+      </View>
+
+      <View>
+        <Text style={{ fontSize: 40, fontWeight: "600" }}>Age: {age}</Text>
+        <TextInput
+          multiline
+          keyboardType="numeric"
+          maxLength={2}
+          onChangeText={(value) => setAge(+value)}
+          style={{
+            borderColor: "green",
+            borderWidth: 1,
+            width: 200,
+            padding: 15,
+          }}
+        />
+      </View>
+
       <Text style={{ fontSize: 40, fontWeight: "600" }}>{count}</Text>
       <View style={{ flexDirection: "row", gap: 10 }}>
         <Button
