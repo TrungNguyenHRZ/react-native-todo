effd2835ae2caba0076cfd87748723fc527dd53c
TrungNguyenHRZ <trungnt10012k@gmail.com>
2025-07-15 16:17:19 +0700

    use array in app

--- App.tsx
+++ App.tsx
@@ -1,65 +1,78 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import {
+  Button,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 export default function App() {
-  const [count, setCount] = useState<number>(0);
-  const [name, setName] = useState<string>("");
-  const [age, setAge] = useState<number>(0);
+  const [students, setStudents] = useState([
+    {
+      id: 1,
+      name: "Eric",
+      age: 18,
+    },
+    {
+      id: 2,
+      name: "Eric1",
+      age: 18,
+    },
+    {
+      id: 3,
+      name: "Eric2",
+      age: 18,
+    },
+    {
+      id: 4,
+      name: "Eric3",
+      age: 18,
+    },
+    {
+      id: 5,
+      name: "Eric4",
+      age: 18,
+    },
+    {
+      id: 6,
+      name: "Eric5",
+      age: 18,
+    },
+  ]);
 
   return (
     <View style={styles.container}>
-      <View>
-        <Text style={{ fontSize: 40, fontWeight: "600" }}>Name: {name}</Text>
-        <TextInput
-          multiline
-          autoCapitalize={"words"}
-          onChangeText={(value) => setName(value)}
-          style={{
-            borderColor: "green",
-            borderWidth: 1,
-            width: 200,
-            padding: 15,
-          }}
-        />
-      </View>
-
-      <View>
-        <Text style={{ fontSize: 40, fontWeight: "600" }}>Age: {age}</Text>
-        <TextInput
-          multiline
-          keyboardType="numeric"
-          maxLength={2}
-          onChangeText={(value) => setAge(+value)}
-          style={{
-            borderColor: "green",
-            borderWidth: 1,
-            width: 200,
-            padding: 15,
-          }}
-        />
-      </View>
-
-      <Text style={{ fontSize: 40, fontWeight: "600" }}>{count}</Text>
-      <View style={{ flexDirection: "row", gap: 10 }}>
-        <Button
-          title="trừ"
-          onPress={() => setCount(count - 1)}
-          disabled={count <= 0}
-        />
-        <Button title="cộng" onPress={() => setCount(count + 1)} />
-        <Button title="Mặc định" onPress={() => setCount(1)} />
-      </View>
+      <Text style={{ fontSize: 60 }}>Hello world</Text>
+      <ScrollView>
+        {students.map((item) => {
+          return (
+            <View
+              key={item.id}
+              style={{
+                padding: 15,
+                backgroundColor: "pink",
+                marginBottom: 80,
+              }}
+            >
+              <Text>{item.name}</Text>
+            </View>
+          );
+        })}
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: 50,
+    paddingHorizontal: 20,
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    // alignItems: "center",
+    // justifyContent: "center",
   },
-  countButton: {},
 });
